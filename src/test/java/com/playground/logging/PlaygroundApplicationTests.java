a588127acf35da85733f2d71076cdbd3b1e8442c
jylee <jylee.tech@gmail.com>
2021-11-01 12:02:31 +0900

    first commit

---
+++ src/test/java/com/playground/logging/PlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playground.logging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
